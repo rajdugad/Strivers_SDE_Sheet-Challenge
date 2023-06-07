2642e5638b0b25d8336991e3021ebd972d0a0cc1
Raj Dugad <rajdugad4321@gmail.com>
2023-06-07 14:57:30 +0530

    adding revere linklist

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    LinkedListNode<int> *p=NULL,*q=NULL,*r=head;
+    while(r){
+        p=q;
+        q=r;
+        r=r->next;
+        q->next=p;
+    }
+    return q;
+
+}
